6c801ee3968f894539855204242971d834a06540
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-09-02 23:55:08 -0600

    chore(login): editing login page

--- src/app/iniciar-sesion/page.tsx
+++ src/app/iniciar-sesion/page.tsx
@@ -5,6 +5,7 @@ function LoginPage() {
     <main style={{ padding: "2rem", color: "var(--color-text)" }}>
       <h1>Iniciar sesión</h1>
       <h1>Correo electrónico o usuario</h1>
+      <h1>Contraseña</h1>
       <p>Página en construcción</p>
       <Link href={"/"} style={{ color: "var(--color-brand" }}>
         Volver al menu
